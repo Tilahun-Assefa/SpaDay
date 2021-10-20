95db5fd65ba9a67f2f56a152ee4e9480c044b7f1
Tilahun Demissie <til.assf98@gmail.com>
2021-10-20 14:04:29 -0500

    15.7 Studio spa user validation done

---
+++ SpaDay/ViewModels/AddUserViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaDay.ViewModels
+{
+    public class AddUserViewModel
+    {
+        [Required(ErrorMessage ="User name is required")]
+        [StringLength(15, MinimumLength =5, ErrorMessage ="User name must be between 5 and 15 characters long.")]
+        public string Username { get; set; }
+
+        
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Sorry, but the given password is too short. Passwords must be at least 6 characters long.")]
+        public string Password { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string VerifyPassword { get; set; }
+
+    }
+}
